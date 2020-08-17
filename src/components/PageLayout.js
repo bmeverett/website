0264c4c62537f2d24fcd3dcb1145632f1fe35f6c
Brandon Everett <bme2010@gmail.com>
2020-08-17 14:10:06 -0400

    don't display scroll bars on pages that don't need them

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -4,9 +4,13 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 export default ({ children }) => (
-  <Container fluid className="px-0 theme-light app-container">
+  <Container
+    fluid
+    className="px-0 theme-light app-container min-vh-100"
+    style={{ overflow: "hidden" }}
+  >
     <Header />
-    <Container fluid className="pt-5 mt-5 text-center min-vh-100">
+    <Container fluid className="pt-5 mt-5 text-center">
       {children}
     </Container>
     <Footer />
